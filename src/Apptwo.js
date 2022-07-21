ae42eb60daf396a64362e94d8dbb080fefe5e1b7
kiBROm <danu@localhost.localdomain>
2022-07-21 01:44:27 -0400

    usereduce review 2

--- src/Apptwo.js
+++ src/Apptwo.js
@@ -2,9 +2,10 @@
 import {useReducer, useState} from "react"
 import Todo from "./Todo"
 
-const ACTION = {
-    ADDTODO : "addtodo",
-    TOGGLETODO : "toggletodo",
+export const ACTION = {
+    ADDTODO : "add_todo",
+    TOGGLETODO : "toggle_todo",
+    DELETETODO : "delete_todo"
 }
 
 function reducer (todos , action) {
@@ -13,12 +14,23 @@ function reducer (todos , action) {
     case ACTION.ADDTODO :    
      return [...todos.reverse() , createTodo(action.payload.todo_txt)].reverse()
 
+    case ACTION.TOGGLETODO :
+      return todos.map(todo => { 
+          if(todo.id === action.payload.id) 
+          { return {...todo , complete : !todo.complete }}
+          return todo
+      })
 
+    case ACTION.DELETETODO : 
+      return todos.filter(todo => todo.id !== action.payload.id)
+
+    default: 
+      return todos
     }
 
 }
 
-function createTodo(todo) { console.log(Date.now())
+function createTodo(todo) { 
     return {id : Date.now() , text : todo , complete : false}
 }
 
@@ -27,11 +39,16 @@ export default function Apptwo() {
     const [todo , settodo] = useState("")
     const [todos , dispatch] = useReducer(reducer , [])
 
+    const addTodo = () => { 
+        settodo("");
+        dispatch({type : ACTION.ADDTODO, payload : {todo_txt : todo}})
+    }
+
     console.log(todos)
     return (<article>
         <input value={todo} type = "text" onChange = { e => settodo(e.target.value)}/>
-        <button onClick={(() => dispatch({type : ACTION.ADDTODO, payload : {todo_txt : todo}}))}>+</button>
+        <button onClick={(() => addTodo())}>+</button>
 
-        {todos.map(el => { return <Todo key={el.id} todo={el}/>})}
+        {todos.map(el => { return <Todo key={el.id} todo={el} dispatch={dispatch} settodo={settodo}/>})}
         </article>)
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,16 @@
+import {ACTION} from "./Apptwo"
+ 
+ export default function Todo({todo, dispatch, settodo}) {
 
- export default function Todo({todo}) {
+    const edittodo = () => {
+        settodo(todo.text)
+        dispatch({type : ACTION.DELETETODO , payload : {id : todo.id}})
+    }
 
     return (<section>
         <p style={{color : todo.complete ? "black" : "gray"}}>{todo.text}</p>
-        <button>Toggle</button>
-        <button>Delete</button>
-
+        <button onClick={() => dispatch({type : ACTION.TOGGLETODO , payload : {id : todo.id}})}>Toggle</button>
+        <button onClick={() => dispatch({type : ACTION.DELETETODO , payload : {id : todo.id}})}>Delete</button>
+        <button onClick={() => edittodo()}>Edit</button>
         </section>)
  }
